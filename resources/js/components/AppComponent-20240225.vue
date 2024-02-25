377903beeb4d2c7f940842d879585dbd9ef047b5
Md.Samiul <jira.operations@gmail.com>
2024-02-25 18:24:05 +0600

    try different componenet

---
+++ resources/js/components/AppComponent-20240225.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app_wrapper">
+    <router-view />
+  </div>
+</template>
+<script>
+import { mapActions } from "vuex";
+
+export default {
+    methods: {
+        ...mapActions('authUser', ['authCheck']),
+    },
+    created() {
+        this.authCheck();
+    }
+};
+</script>
+<style>
+body{
+  background: #e8f7f0;
+}
+.app_wrapper{
+  display: block;
+  position: relative;
+}
+</style>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -4,13 +4,17 @@ import AboutView from '../views/AboutView.vue';
 import TodoView from '../views/TodoView.vue';
 import UserProfileView from '../views/UserProfileView.vue';
 import BlogView from "../views/BlogView.vue";
+import AdminLayout from "../views/AdminLayout.vue";
+import WebLayout from "../views/WebLayout.vue";
 
 const routes = [
-   { path: '/',  name: 'HomeView', component: HomeView },
+   { path: '/',  name: 'HomeView', component: HomeView, meta: { requiresAuth: true } },
    { path: '/vue/about', name: 'AboutView', component: AboutView },
    { path: '/vue/todo', name: 'TodoView', component: TodoView },
    { path: '/vue/user/profile', name: 'UserProfileView', component: UserProfileView},
    { path: '/vue/blog', name: 'BlogView', component: BlogView},
+   { path: '/vue/admin',  name: 'AdminLayout', component: AdminLayout, meta: { requiresAuth: true } },
+   { path: '/vue/web',  name: 'WebLayout', component: WebLayout},
 ];
 
 const router = createRouter({
